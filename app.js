e64d6f3649a316d355cf8abf874a26e470cc7f61
DomZzz <domzzz@DomZzzs-MacBook-Pro.local>
2025-11-07 17:02:38 +0700

    Fix Firebase bucket domain and add auth/storage setup

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD3kI3RlOIeGd4r5evCwjpzDe-47utaWco",
   authDomain: "pl-tbk-file.firebaseapp.com",
   projectId: "pl-tbk-file",
-  storageBucket: "pl-tbk-file.firebasestorage.app",
+  storageBucket: "pl-tbk-file.appspot.com",
   messagingSenderId: "392294785491",
   appId: "1:392294785491:web:479dddf03d5ded40bf4f26"
 };
